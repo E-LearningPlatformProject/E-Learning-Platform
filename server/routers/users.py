4bad3f4a0e6d784ccd1244a1e0eee2ef5b1aa313
petrovradostin <radostin.petrov15@gmail.com>
2024-05-19 21:24:13 +0300

    add register students/teachers and login

--- server/routers/users.py
+++ server/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Header
 from common.auth import get_user_or_raise_401
 from common.responses import BadRequest
-from data.models import LoginData,User
+from data.models import LoginData, Students, Teachers, User
 from services import users_service
 
 
@@ -10,7 +10,7 @@ users_router = APIRouter(prefix='/users')
 
 @users_router.post('/login')
 def login(data: LoginData):
-    user = users_service.try_login(data.username, data.password)
+    user = users_service.try_login(data.email, data.password)
 
     if user:
         token = users_service.create_token(user)
@@ -26,17 +26,34 @@ def user_info(x_token: str | None = Header()):
     return get_user_or_raise_401(x_token)
 
 
-@users_router.post('/register')
-def register(data: User):
+@users_router.post('/register/student')
+def register(user_data: User, student_data: Students):
 
-    if data.email and data.username and data.password:
-        user = users_service.create(data.username, data.password, data.email)
+    if user_data.email and user_data.password:
+        user = users_service.create_student(user_data.email, user_data.password, student_data.first_name, student_data.last_name)
     else:
-        if not data.email:
+        if not user_data.email:
             return BadRequest(content= 'Email should contain symbol "@" and at least one full stop "."')
-        if not data.username:
-            return BadRequest(content= 'Username should be between 2 and 30 symbols.')
-        if not data.password:
+        if not user_data.password:
             return BadRequest(content= 'Password should be between 6 and 30 symbols.')
     
-    return user or BadRequest(f'Username {data.username} is taken.')
+    return user or BadRequest(f'Email {user_data.email} is taken.')
+
+
+@users_router.post('/register/teacher')
+def register(user_data: User, teacher_data: Teachers):
+
+    if user_data.email and user_data.password:
+        user = users_service.create_teacher(user_data.email,
+                                            user_data.password,
+                                            teacher_data.first_name, 
+                                            teacher_data.last_name,
+                                            teacher_data.phone_number,
+                                            teacher_data.linked_in_account)
+    else:
+        if not user_data.email:
+            return BadRequest(content= 'Email should contain symbol "@" and at least one full stop "."')
+        if not user_data.password:
+            return BadRequest(content= 'Password should be between 6 and 30 symbols.')
+    
+    return user or BadRequest(f'Email {user_data.email} is taken.')
