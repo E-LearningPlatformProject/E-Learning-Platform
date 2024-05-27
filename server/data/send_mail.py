0caf26814bf60385bd58759f8182919229fe54a1
romadimitrov <rdimitrov877@gmail.com>
2024-05-27 10:32:29 +0300

    Create functions to send multiple emails

--- server/data/send_mail.py
+++ server/data/send_mail.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+from email.mime.text import MIMEText
 from os import getenv
 
 
@@ -21,3 +22,49 @@ Subject: Approval needed\n
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
+
+
+def send_multiple_email(recipients_emails:list[str], user_email:str, user_id:int):
+    host = 'smtp.gmail.com'
+    port = 587
+
+    username = getenv('EMAIL')
+    password =getenv('APPKEY_eLearning')
+
+    #recipients = " .".join(recipients)
+    recipients = [
+   "rdimitrov877@gmail.com",
+   "rd94@icloud.com"
+   ]
+
+    message = MIMEText(f'''
+        User with ID: user_id. and email user_email waiting for approval!
+        You can approve his register with click on the link
+        ''')
+    message['Subject'] = 'Aproval needed'
+    message["From"] = username
+    message["To"] = " .".join(recipients)
+
+    server = smtplib.SMTP(host, port)
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(username, recipients, message.as_string())
+    server.quit()
+
+
+
+#recipients = [
+#   "example@example.com",
+#   "example1@example.com",
+#   "example2@example.com",
+#]
+#s = smtplib.SMTP("smtp.gmail.com", 587)
+#s.starttls()
+#s.login("yourmail@gmail.com", "password")
+#msg = MIMEText("""your body text""")
+#sender = "yourmail@gmail.com"
+#msg["Subject"] = "Mail subject"
+#msg["From"] = sender
+#msg["To"] = " .".join(recipients)
+#s.sendmail(sender, recipients, msg.as_string())
+#s.quit()
